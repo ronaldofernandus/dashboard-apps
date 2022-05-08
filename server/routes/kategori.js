fe02a044cfa9174c7eee0df7f9f34132c5fef824
ronaldofernandus <ronaldofernandus10@gmail.com>
2022-05-08 15:08:40 +0700

    backend-end

--- server/routes/kategori.js
+++ server/routes/kategori.js
@@ -3,9 +3,9 @@ const { kategoriController } = require("../controllers");
 
 kategoriRoute.get("/", kategoriController.getKategori);
 kategoriRoute.post("/add", kategoriController.tambahKategori);
-kategoriRoute.get("/add", kategoriController.tambahKategoriPage);
-kategoriRoute.post("/edit/:id", kategoriController.editKategori);
-kategoriRoute.get("/edit/:id", kategoriController.editKategoriPage)
-kategoriRoute.get("/delete/:id", kategoriController.deleteKategori);
+
+kategoriRoute.put("/edit/:id", kategoriController.editKategori);
+
+kategoriRoute.delete("/delete/:id", kategoriController.deleteKategori);
 
 module.exports = kategoriRoute;
